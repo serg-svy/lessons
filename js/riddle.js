a18b11c5a6631cfeab3f972b43bbd1a6c1f0d984
serg-svy <97796536+serg-svy@users.noreply.github.com>
2024-02-16 17:53:28 +0200

    up

---
+++ js/riddle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function check(riddle, answer) {
+    const userInput = prompt(riddle);
+
+    return userInput ? answer.toLowerCase() === userInput.toLowerCase() : "Vi ne vveli ni chego";
+}
+
+console.log(check('Dage ne samolet, a s krilyami', 'samolet'));
+console.log(check('Cherez vodu on provodit', ' most'));
